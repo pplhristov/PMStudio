87f9e1707a01cf043e8f94ac66115fa618f4c61a
Petar <Petar@DESKTOP-E589U5A>
2020-12-20 22:21:46 -0800

    Various view improvements

--- CSharp-ASP.NET Core-PMS/Data/PMStudio.Data.Models/PropertyVendor.cs
+++ CSharp-ASP.NET Core-PMS/Data/PMStudio.Data.Models/PropertyVendor.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PMStudio.Data.Models
+﻿namespace PMStudio.Data.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
     public class PropertyVendor
     {
         public int Id { get; set; }
