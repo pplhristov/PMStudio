de14c70215bd1e8061f08ffa81596dafc07476fc
Petar <Petar@DESKTOP-E589U5A>
2021-08-15 17:43:18 -0700

    Add PropertiesServices create test

--- Tests/PMStudio.Services.Data.Tests/PropertiesServiceTest.cs
+++ Tests/PMStudio.Services.Data.Tests/PropertiesServiceTest.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using PMStudio.Data;
 using PMStudio.Data.Models;
 using PMStudio.Data.Repositories;
+using PMStudio.Web.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,7 @@ using Xunit;
 
 namespace PMStudio.Services.Data.Tests
 {
-   public class PropertiesServiceTest
+    public class PropertiesServiceTest
     {
         [Fact]
         public async Task GetCountShouldReturnCorrectNumberUsingDbContext()
@@ -27,6 +29,72 @@ namespace PMStudio.Services.Data.Tests
             using var propertyRepository = new EfDeletableEntityRepository<Property>(dbContext);
             var propertiesService = new PropertiesService(propertyRepository);
             Assert.Equal(3, propertiesService.GetCount());
+        }
+
+        [Fact]
+        public async Task CreateShouldSucceed()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase(databaseName: "CreatePropertiesTestDb").Options;
+            using var dbContext = new ApplicationDbContext(options);
+            using var propertyRepository = new EfDeletableEntityRepository<Property>(dbContext);
+            var propertiesService = new PropertiesService(propertyRepository);
+
+            var model = new CreatePropertiesViewModel()
+            {
+                Address = "Test",
+                Name = "Test",
+                Owner = "Owner",
+                Size = 110,
+                Type = PMStudio.Data.Models.Enum.PropertyType.Residential,
+                Images = new List<IFormFile>(),
+            };
+
+            await propertiesService.CreateAsync(model, @"C:\PM.Studio.Images");
+
+            var createdModel = dbContext.Properties.FirstOrDefault(p => p.Name == "Test");
+
+            Assert.NotNull(createdModel);
+        }
+
+        [Fact]
+        public async Task GetByIdShouldReturnTheCorrectProperty()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "GetByIdPropertiesTestDb").Options;
+            using var dbContext = new ApplicationDbContext(options);
+
+            var propertyId = 5;
+            var propertyName = $"Property with ID {propertyId}";
+
+            var manager = new ApplicationUser()
+            {
+                UserName = "Admin",
+            };
+
+            var tenant = new Tenant()
+            {
+                Name = "Test tenant",
+            };
+
+            dbContext.Properties.Add(new Property()
+            {
+                Id = propertyId,
+                Name = propertyName,
+                Type = PMStudio.Data.Models.Enum.PropertyType.Residential,
+                Address = "Kings Lynn",
+                Manager = manager,
+                Owner = "Test owner",
+                Tenant = tenant,
+                ModifiedOn = DateTime.Now.AddDays(+1),
+            });
+            await dbContext.SaveChangesAsync();
+
+            using var propertyRepository = new EfDeletableEntityRepository<Property>(dbContext);
+            var propertiesService = new PropertiesService(propertyRepository);
+            var property = propertiesService.GetById<SinglePropertyViewModel>(propertyId);
+
+            Assert.Equal(propertyName, property.Name);
         }
     }
 }
