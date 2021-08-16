571536c3ca74fdf337ff5bc79f7d127b6321cb3c
Petar <Petar@DESKTOP-E589U5A>
2021-08-15 17:02:38 -0700

    Add PropertiesService test

---
+++ Tests/PMStudio.Services.Data.Tests/PropertiesServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMStudio.Data;
+using PMStudio.Data.Models;
+using PMStudio.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PMStudio.Services.Data.Tests
+{
+   public class PropertiesServiceTest
+    {
+        [Fact]
+        public async Task GetCountShouldReturnCorrectNumberUsingDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "PropertiesTestDb").Options;
+            using var dbContext = new ApplicationDbContext(options);
+            dbContext.Properties.Add(new Property());
+            dbContext.Properties.Add(new Property());
+            dbContext.Properties.Add(new Property());
+            await dbContext.SaveChangesAsync();
+
+            using var propertyRepository = new EfDeletableEntityRepository<Property>(dbContext);
+            var propertiesService = new PropertiesService(propertyRepository);
+            Assert.Equal(3, propertiesService.GetCount());
+        }
+    }
+}
